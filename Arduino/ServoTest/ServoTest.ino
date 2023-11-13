54161ec51d3eaa361101d75ec53bd2d63b9a67ae
everetttt <ekernen2@gmail.com>
2023-11-13 16:48:25 -0600

    changed servo blocking to integers rather than servo arrays for simplicity and added foundation for placing a row of pixels at a time

--- Arduino/ServoTest/ServoTest.ino
+++ Arduino/ServoTest/ServoTest.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 
 /* --- SERVO VARIABLES --- */
+const int numServos = 7;
 Servo col1; const int col1Pin = 5;  //columns go left to right when viewing from front
 Servo col2; const int col2Pin = 6;
 Servo col3; const int col3Pin = 7;
@@ -8,44 +9,50 @@ Servo col4; const int col4Pin = 8;
 Servo col5; const int col5Pin = 9;
 Servo col6; const int col6Pin = 10;
 Servo col7; const int col7Pin = 11;
-Servo servos[7] = { col1, col2, col3, col4, col5, col6, col7 };
+Servo servos[numServos] = { col1, col2, col3, col4, col5, col6, col7 };
+int blockCol[numServos];
+int blockIndex = 0;
 
-/* --- OTHER VARIABLES --- */
-int closedPosition = 90;  //angle of servo when no pixels may pass
-int openPosition = 0;     //angle of servo when pixel should pass
-int timeOpen = 400;      //time spent open, calibrated to only allow one pixel to pass
+/* --- GATE VARIABLES --- */
+const int closedPosition = 90;  //angle of servo when no pixels may pass
+const int openPosition = 0;     //angle of servo when pixel should pass
+const int timeOpen = 400;      //time spent open, calibrated to only allow one pixel to pass
+const int timeBetweenRows = 2500; //replace with limit switch eventually
 
-
+/* --- SETUP FUNCTION --- */
 void setup() {
   Serial.begin(9600);
   ServoWrite();
-  delay(9000);
 }
-
+/* --- LOOP FUNCTION --- */
 void loop() {
-  //analyze image and decide which pixels to place
+  //analyze image and decide which columns to block, then call PlacePixel
+  /*blockIndex = 0;
+  for (int i = 0; i < number of columns blocking; i++) {
+    blockCol[i] = column index
+    blockIndex++;
+  }*/
 
-  /*for (int index = 0; index < sizeof(servos); index++) {
-    Serial.println(index);
-    Servo servo[] = {servos[index]};
-    PlacePixel(servo);
-  }
-  */
-  PlacePixel(servos);
-  delay(2500);
+  PlacePixel();
+  delay(timeBetweenRows);
   
 }
+
 /* allow one pixel to pass for each servo specified, then close all servos */
-void PlacePixel(Servo servo[]) {
-  for (int index = 0; index < sizeof(servo); index++) {
-    servo[index].write(openPosition);
+void PlacePixel() {
+  Serial.print("Releasing row. Blocking columns: ");
+  for (int i = 0; i < blockIndex; i++) {
+    servos[blockCol[i]].write(openPosition);
   }
   delay(timeOpen);
-  for (int index = 0; index < sizeof(servo); index++) {
-    servo[index].write(closedPosition);
+  for (int i = 0; i < blockIndex; i++) {
+    servos[blockCol[i]].write(closedPosition);
   }
 }
-/* --- Setup Code, used to reduce the size of the setup function --- */
+
+/* --- HELPER FUNCTIONS --- */
+
+// Setup Code, used to reduce the size of the setup function
 void ServoWrite() {
   Serial.println("Entered Servo Setup");
   col1.attach(col1Pin);
@@ -56,8 +63,8 @@ void ServoWrite() {
   col6.attach(col6Pin);
   col7.attach(col7Pin);
 
-  for (int index = 0; index < sizeof(servos); index++) {
+  for (int index = 0; index < numServos; index++) {
     servos[index].write(closedPosition);
   }
-  Serial.println("Finished Servo Setup");
+  Serial.println("Finished Servo Setup and initialized all to neutral position");
 }
