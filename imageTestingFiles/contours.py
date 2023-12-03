fd80d2c2fce83be3452c80f2e255892691eab16a
qt022302 <115834730+qt022302@users.noreply.github.com>
2023-12-03 14:37:32 -0600

    More contour work (unfinished)
    Pushing some work I did testing the pointPolygonTest so I can test this on other devices

--- imageTestingFiles/contours.py
+++ imageTestingFiles/contours.py
@@ -18,12 +18,13 @@ while True:
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
     # Draw contours on original image
-    #image_copy= frame.copy()
-    #cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0,255,0))
+    image_copy= frame.copy()
+    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0,255,0))
 
     # Draw contours on blank image
-    blank_img = np.zeros((720, 1080, 3), dtype = np.uint8)
+    blank_img = np.zeros((frame.shape[0], frame.shape[1], 3), dtype = np.uint8)
     cv2.drawContours(image=blank_img, contours=contours, contourIdx=-1, color=(0,255,0))
+
     # Show
     cv2.imshow('None contour on black', blank_img)
     #cv2.imshow('None contour', image_copy)
@@ -32,6 +33,23 @@ while True:
     #cv2.imshow('Color Frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
+        for i in range(frame.shape[0]):
+            for j in range (frame.shape[1]):
+                test_val = cv2.pointPolygonTest(contours[0], (j, i), True)
+                if test_val > 0:
+                    blank_img[i,j,0] = 255
+                    blank_img[i,j,1] = 255
+                    blank_img[i,j,2] = 255
+                elif test_val < 0:
+                    blank_img[i,j,0] = 0
+                    blank_img[i,j,1] = 0
+                    blank_img[i,1,2] = 0
+                elif test_val == 0:
+                    blank_img[i,j,0] = 0
+                    blank_img[i,j,1] = 255
+                    blank_img[i,j,2] = 0
+        #cv2.imwrite("contours.png", blank_img_copy)
+        cv2.imwrite("pointPolygonTest.png", blank_img)
         break
 
 # After the loop release the cap object 
