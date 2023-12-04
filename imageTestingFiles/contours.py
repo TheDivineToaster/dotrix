3e9679764222aa06b326391de3d5316497584232
qt022302 <qt022302@gmail.com>
2023-12-04 17:53:14 -0600

    Further contour work
    Made more progress, it should be able to detect all contours, then calculate the longest one and fill it in with white.

--- imageTestingFiles/contours.py
+++ imageTestingFiles/contours.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 vid = cv2.VideoCapture(0)
 
@@ -18,38 +19,47 @@ while True:
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
     # Draw contours on original image
-    image_copy= frame.copy()
-    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0,255,0))
+    #image_copy= frame.copy()
+    #cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0,255,0))
 
     # Draw contours on blank image
-    blank_img = np.zeros((frame.shape[0], frame.shape[1], 3), dtype = np.uint8)
-    cv2.drawContours(image=blank_img, contours=contours, contourIdx=-1, color=(0,255,0))
-
-    # Show
-    cv2.imshow('None contour on black', blank_img)
-    #cv2.imshow('None contour', image_copy)
+    all_contours_image = np.zeros((frame.shape[0], frame.shape[1], 3), dtype = np.uint8)
+    cv2.drawContours(image=all_contours_image, contours=contours, contourIdx=-1, color=(0,255,0))
+    
+    # Draw the longest contour on a different blank image
+    max_contour_image = np.zeros((frame.shape[0], frame.shape[1], 3), dtype = np.uint8)
+    if len(contours) > 1:
+        max_contour = max(contours, key = len)
+        cv2.drawContours(image=max_contour_image, contours=max_contour, contourIdx=-1, color=(255,255,255))
+    
+    # Show live feeds (feel free to comment and uncomment with what you need to see)
+    cv2.imshow('All contours', all_contours_image)
+    cv2.imshow('Just one contour', max_contour_image)
+    #cv2.imshow('All contours over image', image_copy)
     #cv2.imshow('Threshold binary image', thresh)
     #cv2.imshow('Grayscale Frame', img_gray)
     #cv2.imshow('Color Frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Write original image, image of all contours, image of longest contour
+        cv2.imwrite("original_image.png", frame)
+        cv2.imwrite("contours.png", all_contours_image)
+        cv2.imwrite("longest_contour.png", max_contour_image)
+        # Fill in longest contour with white
+        timestamp_1 = time.time()
         for i in range(frame.shape[0]):
             for j in range (frame.shape[1]):
-                test_val = cv2.pointPolygonTest(contours[0], (j, i), True)
+                test_val = cv2.pointPolygonTest(max_contour, (j, i), True)
+                # If it's in the contour, color it white
                 if test_val > 0:
-                    blank_img[i,j,0] = 255
-                    blank_img[i,j,1] = 255
-                    blank_img[i,j,2] = 255
-                elif test_val < 0:
-                    blank_img[i,j,0] = 0
-                    blank_img[i,j,1] = 0
-                    blank_img[i,1,2] = 0
-                elif test_val == 0:
-                    blank_img[i,j,0] = 0
-                    blank_img[i,j,1] = 255
-                    blank_img[i,j,2] = 0
-        #cv2.imwrite("contours.png", blank_img_copy)
-        cv2.imwrite("pointPolygonTest.png", blank_img)
+                    max_contour_image[i,j,0] = 255
+                    max_contour_image[i,j,1] = 255
+                    max_contour_image[i,j,2] = 255
+        timestamp_2 = time.time()
+        total_time = timestamp_2 - timestamp_1
+        print("Filling in contours took " + str(total_time) + " seconds.")
+        # Write image of longest contour filled in
+        cv2.imwrite("pointPolygonTest.png", max_contour_image)
         break
 
 # After the loop release the cap object 
