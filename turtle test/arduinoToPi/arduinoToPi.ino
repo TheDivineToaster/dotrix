5364f76329ced144d22c532165a342f6b71e3af5
everetttt <ekernen2@gmail.com>
2023-12-07 16:29:42 -0600

    for some reason turtle is slowing things down tremendously

--- turtle test/arduinoToPi/arduinoToPi.ino
+++ turtle test/arduinoToPi/arduinoToPi.ino
@@ -1,37 +1,37 @@
 /* Pin Names and Numbers */
-const int vertPot = A0;
-const int horizPot = A2;
+const int horizPot = A0;
+const int vertPot = A2;
 const int speedPot = A4;
 
 
 char data[15] = {0};
-char vert[25] = {0};
 char horiz[25] = {0};
+char vert[25] = {0};
 char speed[25] = {0};
 
 void setup() {
   Serial.begin(9600);           //Starting serial communication
 
-  pinMode(vertPot, INPUT);
   pinMode(horizPot, INPUT);
+  pinMode(vertPot, INPUT);
   pinMode(speedPot, INPUT);
   
 }
   
 void loop() {
-  int vertVal = analogRead(vertPot);
   int horizVal = analogRead(horizPot);
+  int vertVal = analogRead(vertPot);
   int speedVal = analogRead(speedPot);
-
-  itoa(vertVal, data, 10);
-  strcpy(vert, "Vert: ");
-  strcat(vert, data);
-  Serial.println(vert);
 
   itoa(horizVal, data, 10);
   strcpy(horiz, "Horiz: ");
   strcat(horiz, data);
   Serial.println(horiz);
+
+  itoa(vertVal, data, 10);
+  strcpy(vert, "Vert: ");
+  strcat(vert, data);
+  Serial.println(vert);
 
   itoa(speedVal, data, 10);
   strcpy(speed, "Speed: ");
--- turtle test/laptop_serial_test.py
+++ turtle test/laptop_serial_test.py
@@ -6,46 +6,72 @@ from turtle import * # https://docs.python.org/3/library/turtle.html
 COM_PORT = 'COM6'
 BAUDRATE = 9600
 
+global x_pot
 x_pot = 0   # can have values between 0 and 1023
+global y_pot
 y_pot = 0
+global speed_pot
 speed_pot = 0
 
 
 ser = serial.Serial(COM_PORT, BAUDRATE)
 s = [0,1]
 
-now = datetime.now()
-freq = 2 #how often do you want to print the outputs?
+global turtle_time
+turtle_time = datetime.now()
+turtle_freq = 0.1
+
+global print_freq
+print_freq = 0.75 #how often do you want to print the outputs?
 
 def mainloop():
-    now = datetime.now()
+    print_now = datetime.now()
     while True:
         read_serial = ser.readline()
         data = read_serial.decode().strip()
-        parsedata(data)
-        if datetime.now() > now + timedelta(seconds=freq):
-            now = datetime.now()
-            x_print = "Vert: " + str(x_pot)
-            y_print = "Horiz: " + str(y_pot)
+
+        data_split = data.split(' ')
+
+        setpots(data_split)
+        
+        if datetime.now() > turtle_time + timedelta(seconds=turtle_freq):
+            turtle()
+
+
+        if datetime.now() > print_now + timedelta(seconds=print_freq):
+            print_now = datetime.now()
+            x_print = "Horiz: " + str(x_pot)
+            y_print = "Vert: " + str(y_pot)
             speed_print = "Speed: " + str(speed_pot)
             #print(f"{x_print : <25}{y_print : ^25}{speed_print : }") 
             print(" %-25s %-25s %-25s" % (x_print, y_print, speed_print))
 
-
-def parsedata(data):
-    data_split = data.split(' ')
-    if data_split[0] == "Vert:":
-        y_pot = int(data_split[1])
-        return
-    elif data_split[0] == "Horiz:":
-        x_pot = int(data_split[1])
-        return
+def setpots(data_split):
+    if data_split[0] == "Horiz:":
+        try:
+            global x_pot
+            x_pot = int(data_split[1])
+            return
+        except:
+            temp = 4 # do nothing
+    elif data_split[0] == "Vert:":
+        try:
+            global y_pot
+            y_pot = int(data_split[1])
+            return
+        except:
+            temp = 4
     elif data_split[0] == "Speed:":
-        speed_pot = int(data_split[1])
-        return
+        try:
+            global speed_pot
+            speed_pot = int(data_split[1])
+            return
+        except:
+            temp = 4
     else:
-        print("reconsider your life")
-        return
+        print("reconsider life")
+    return
+
 
 def turtle():
     x = x_pot - 512     # cut in half so values are between -512 and 512
@@ -55,10 +81,7 @@ def turtle():
     if (x < 0):
         q1 = False
 
-    distance = speed * 0.125
-
-    x /= 512
-    y /= 512
+    distance = speed_pot * 0.00125
 
     angle = math.degrees(math.atan(x / y))
 
