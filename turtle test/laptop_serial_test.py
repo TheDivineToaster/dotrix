7e87f6d8be1342bf46ac0df8127ab614c4701ff5
everetttt <ekernen2@gmail.com>
2023-12-07 17:28:47 -0600

    now turtle doesn't work at all :sunglasses:

--- turtle test/laptop_serial_test.py
+++ turtle test/laptop_serial_test.py
@@ -5,46 +5,42 @@ from turtle import * # https://docs.python.org/3/library/turtle.html
 
 COM_PORT = 'COM6'
 BAUDRATE = 9600
+SER_TIMEOUT = 0.04
 
-global x_pot
-x_pot = 0   # can have values between 0 and 1023
-global y_pot
-y_pot = 0
-global speed_pot
-speed_pot = 0
+ser = serial.Serial(port=COM_PORT, baudrate=BAUDRATE, timeout=SER_TIMEOUT)
 
+x_pot = 512   # pots can have values between 0 and 1023
+y_pot = 512
+speed_pot = 512
 
-ser = serial.Serial(COM_PORT, BAUDRATE)
-s = [0,1]
-
-global turtle_time
 turtle_time = datetime.now()
-turtle_freq = 0.1
+turtle_freq = 0.1   # how often do we call turtle()?
 
-global print_freq
-print_freq = 0.75 #how often do you want to print the outputs?
+print_time = datetime.now()
+print_freq = 0.75   # how often do you want to print the outputs?
 
 def mainloop():
-    print_now = datetime.now()
     while True:
         read_serial = ser.readline()
         data = read_serial.decode().strip()
-
-        data_split = data.split(' ')
-
-        setpots(data_split)
         
-        if datetime.now() > turtle_time + timedelta(seconds=turtle_freq):
-            turtle()
+        if data:
+            data_split = data.split(' ')
+            setpots(data_split)
+            
+            global turtle_time
+            if datetime.now() > turtle_time + timedelta(seconds=turtle_freq):
+                turtle_time = datetime.now()
+                turtle()
+                ser.readlines() # dequeue all serial outputs
 
-
-        if datetime.now() > print_now + timedelta(seconds=print_freq):
-            print_now = datetime.now()
-            x_print = "Horiz: " + str(x_pot)
-            y_print = "Vert: " + str(y_pot)
-            speed_print = "Speed: " + str(speed_pot)
-            #print(f"{x_print : <25}{y_print : ^25}{speed_print : }") 
-            print(" %-25s %-25s %-25s" % (x_print, y_print, speed_print))
+            global print_time
+            if datetime.now() > print_time + timedelta(seconds=print_freq):
+                print_now = datetime.now()
+                x_print = "Horiz: " + str(x_pot)
+                y_print = "Vert: " + str(y_pot)
+                speed_print = "Speed: " + str(speed_pot)
+                print(" %-25s %-25s %-25s" % (x_print, y_print, speed_print))
 
 def setpots(data_split):
     if data_split[0] == "Horiz:":
@@ -83,7 +79,8 @@ def turtle():
 
     distance = speed_pot * 0.00125
 
-    angle = math.degrees(math.atan(x / y))
+    if y != 0:
+        angle = math.degrees(math.atan(x / y))
 
     if (q1):
         angle += 180
@@ -91,10 +88,7 @@ def turtle():
     setheading(angle)
     forward(distance)
 
-
-    # mainloop should dissect the input and separate into x and y values
-    # here, turtle should take the x and y values and set heading as well as speed
-    # auto change color for funsies ig
+    return
 
 
 mainloop()
