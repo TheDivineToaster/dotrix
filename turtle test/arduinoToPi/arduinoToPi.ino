7c482cf444b839e8cd2b5d96bf752181a10d9013
everetttt <ekernen2@gmail.com>
2023-12-07 14:57:56 -0600

    arduino serial test

--- turtle test/arduinoToPi/arduinoToPi.ino
+++ turtle test/arduinoToPi/arduinoToPi.ino
@@ -1,13 +1,40 @@
-char dataString[50] = {0};
-int a =0; 
+/* Pin Names and Numbers */
+const int vertPot = A0;
+const int horizPot = A2;
+const int speedPot = A4;
+
+
+char data[15] = {0};
+char vert[25] = {0};
+char horiz[25] = {0};
+char speed[25] = {0};
 
 void setup() {
   Serial.begin(9600);           //Starting serial communication
+
+  pinMode(vertPot, INPUT);
+  pinMode(horizPot, INPUT);
+  pinMode(speedPot, INPUT);
+  
 }
   
 void loop() {
-  a++;                          // a value increase every loop
-  sprintf(dataString,"%02X",a); // convert a value to hexa 
-  Serial.println(dataString);   // send the data
-  delay(1000);                  // give the loop some break
+  int vertVal = analogRead(vertPot);
+  int horizVal = analogRead(horizPot);
+  int speedVal = analogRead(speedPot);
+
+  itoa(vertVal, data, 10);
+  strcpy(vert, "Vert: ");
+  strcat(vert, data);
+  Serial.println(vert);
+
+  itoa(horizVal, data, 10);
+  strcpy(horiz, "Horiz: ");
+  strcat(horiz, data);
+  Serial.println(horiz);
+
+  itoa(speedVal, data, 10);
+  strcpy(speed, "Speed: ");
+  strcat(speed, data);
+  Serial.println(speed);
 }
---
+++ turtle test/laptop_serial_test.py
@@ -0,0 +1,73 @@
+import math
+from datetime import datetime, timedelta
+import serial
+from turtle import * # https://docs.python.org/3/library/turtle.html
+
+COM_PORT = 'COM6'
+BAUDRATE = 9600
+
+x_pot = 0   # can have values between 0 and 1023
+y_pot = 0
+speed_pot = 0
+
+
+ser = serial.Serial(COM_PORT, BAUDRATE)
+s = [0,1]
+
+now = datetime.now()
+freq = 2 #how often do you want to print the outputs?
+
+def mainloop():
+    now = datetime.now()
+    while True:
+        read_serial = ser.readline()
+        data = read_serial.decode().strip()
+        parsedata(data)
+        if datetime.now() > now + timedelta(seconds=freq):
+            now = datetime.now()
+            x_print = "Vert: " + str(x_pot)
+            y_print = "Horiz: " + str(y_pot)
+            speed_print = "Speed: " + str(speed_pot)
+            #print(f"{x_print : <25}{y_print : ^25}{speed_print : }") 
+            print(" %-25s %-25s %-25s" % (x_print, y_print, speed_print))
+
+
+def parsedata(data):
+    data_split = data.split(' ')
+    if "Vert:" in data_split:
+        y_pot = int(data_split[1])
+    elif "Horiz:" in data_split:
+        x_pot = int(data_split[1])
+    elif "Speed:" in data_split:
+        speed_pot = int(data_split[1])
+    else:
+        print("reconsider your life")
+
+def turtle():
+    x = x_pot - 512     # cut in half so values are between -512 and 512
+    y = y_pot - 512     # use temp variables so they don't get modified by serial (idk if it's an async process)
+
+    q1 = True
+    if (x < 0):
+        q1 = False
+
+    distance = speed * 0.125
+
+    x /= 512
+    y /= 512
+
+    angle = math.degrees(math.atan(x / y))
+
+    if (q1):
+        angle += 180
+
+    setheading(angle)
+    forward(distance)
+
+
+    # mainloop should dissect the input and separate into x and y values
+    # here, turtle should take the x and y values and set heading as well as speed
+    # auto change color for funsies ig
+
+
+mainloop()
