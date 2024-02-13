0e640b92633e90cd615eb4eeb5c7e257f00f6d6b
everetttt <ekernen2@gmail.com>
2024-02-13 12:20:41 -0600

    the issue is that the following does not work:
    I instantiate board as a 2D list of size width x height
    Each element holds 1) a 1 digit integer to represent its color and 2) an array holding all of its neighbors
    Each time the board updates, I want to grab the most common color of the elements in 2) and set 1) to that most common color

--- FluidSim/rock_paper_scissors.py
+++ FluidSim/rock_paper_scissors.py
@@ -1,25 +1,26 @@
 import pygame
 import random
 import numpy
+from collections import Counter
 
 class RockPaperScissors():
 
-    color1 = pygame.Color(228, 26, 28)
-    color2 = pygame.Color(51, 160, 44)
-    color3 = pygame.Color(31, 120, 180)
-    color4 = pygame.Color(231, 41, 138)
+    color1 = (228, 26, 28)
+    color2 = (51, 160, 44)
+    color3 = (31, 120, 180)
+    color4 = (231, 41, 138)
     colors = [color1, color2, color3, color4]
 
 
-    def __init__(self, size_x, size_y, num_colors, starting_density):
+    def __init__(self, size_x, size_y, num_colors):
         self.width = size_x
         self.height = size_y
         self.num_colors = num_colors
-        self.density = starting_density # currently unused
         self.step_count = 0
         self.neighbor_list = None
         self.board = self.board_init()
-        self.screen = numpy.zeros((self.width, self.height))
+        print(self.board)
+        self.screen = numpy.zeros((self.width, self.height, 3))
 
     def board_init(self):
         mid = [[0, 0]]
@@ -33,28 +34,29 @@ class RockPaperScissors():
             for y in range(self.height):
                 xy_color = self.colors[random.randint(1, self.num_colors)]
                 neighbors = []
-                for n, m in possible_neighbors:
-                    if ((x+n) > 0 and (x+n) < self.width and (y+m) > 0 and (y+m) < self.height):
-                        neighbors.append((x+n, y+m))
+                for elem in possible_neighbors:
+                    if x + elem[0] > 0 and x + elem[0] < self.width and y + elem[1] > 0 and y + elem[1] < self.height:
+                        neighbors.append(elem)
 
                 # for edge cells, append themselves enough times to get correct number of neighbors
-                neighbors = neighbors + [(x, y)] * (len(possible_neighbors) - len(neighbors))
+                '''neighbors = neighbors + [(x, y)] * (len(possible_neighbors) - len(neighbors))
                 assert len(neighbors) == len(possible_neighbors)
-                column[y] = (xy_color, neighbors)
+                column[y] = (xy_color, neighbors)'''
             board.append(column)
-    
+        return board
 
     def evolve(self):
         self.step_count += 1
         new_board = self.board.copy()
-        for x in range(self.board):
-            for y in range(self.board[x]):
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
                 curr_state = self.board[x][y][0]
                 neighbors = self.board[x][y][1]
                 states = []
                 for elem in neighbors:
-                    states.append(self.board[x + elem[0]][y + elem[1]])
-                curr_state = max(set(states), key=states.count)
+                    states.append(self.board[x + elem[0]][y + elem[1]][0])
+                counter = Counter(states)
+                curr_state = counter.most_common(1)[0][0]
                 self.screen[x][y] = self.get_color(curr_state)
         self.board = new_board
         return pygame.surfarray.make_surface(self.screen)
@@ -70,14 +72,4 @@ class RockPaperScissors():
             case 4:
                 return self.color4
             case _:
-                return self.color1  # change this later?
-'''  
-    def draw(self, screen, show_step=False):
-        screen.fill(self.background_color)
-        for x in range(self.size_x):
-            for y in range(self.size_y):
-                color = self.color_dict[self.board[(x, y)]]
-                r = self.rects[(x, y)]
-                pygame.draw.rect(screen, color, r, 0)
-
-'''
+                return self.color1  # change this default value later?
--- FluidSim/sim_base.py
+++ FluidSim/sim_base.py
@@ -1,5 +1,7 @@
 import pygame
 import numpy as np
+from rock_paper_scissors import RockPaperScissors
+from fluid_sim import FluidSim
 
 '''Thoughts here are that this script acts as the pygame interpreter, 
 and we have other scripts to control different ways to mess with the image.
@@ -18,12 +20,18 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Create Pygame window
-screen = pygame.display.set_mode((WIDTH * pixels_per_led, HEIGHT * pixels_per_led))
+display = pygame.display.set_mode((WIDTH * pixels_per_led, HEIGHT * pixels_per_led))
 pygame.display.set_caption("Fluid Simulation")
 
 # Initialize grid
 grid = np.zeros((WIDTH, HEIGHT, 3))
 screen = pygame.surfarray.make_surface(grid) # default screen
+
+# Choose which sim we use
+game_select = np.random.randint(0,1) # inclusive
+game_select = 0 # hard set to test rock_paper_scissors
+if (game_select == 0):
+    game = RockPaperScissors(WIDTH, HEIGHT, 3) # grid size and number of colors
 
 # Main loop
 clock = pygame.time.Clock()
@@ -34,15 +42,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    
-    screen.blit(pygame.transform.scale(screen, (WIDTH * pixels_per_led, HEIGHT * pixels_per_led)), (0, 0))
+    screen = game.evolve()
+    display.blit(pygame.transform.scale(screen, (WIDTH * pixels_per_led, HEIGHT * pixels_per_led)), (0, 0))
 
     # Update the display
     pygame.display.flip()
 
     # Cap the frame rate
     clock.tick(FPS)
-    print(clock.get_fps())
+    #print(clock.get_fps())
 
 # Quit Pygame
 pygame.quit()
