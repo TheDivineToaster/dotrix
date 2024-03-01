eb6ce11183c6e80592b3aa5b10d0023616bdfa39
everetttt <ekernen2@gmail.com>
2024-03-01 11:10:22 -0600

    shuffled files around and removed rock_paper_scissors since we have multi_state_life

--- FluidSim/fluid_sim.py
+++ FluidSim/fluid_sim.py
@@ -1,59 +1,150 @@
+from functools import reduce
+from itertools import cycle
+from math import factorial
+from scipy.ndimage import map_coordinates, spline_filter
+from scipy.sparse.linalg import factorized
+from PIL import Image
+from scipy.special import erf
+
 import numpy as np
-import pygame
+import scipy.sparse as sp
+
+# modified from GregTJ's Stable Fluids project
+# https://github.com/GregTJ/stable-fluids
+# Unlicense license
+
+#region FluidSim class
 
 class FluidSim():
-    def __init__(self, size_x, size_y, pixels_per_led, FPS):
-        self.width = size_x
-        self.height = size_y
-        self.pixels_per_led = pixels_per_led
-        self.FPS = FPS
-        self.grid = np.zeros((self.width, self.height, 3), dtype=np.float32)
-        self.screen = (self.grid * 255).astype(np.uint8)
+    def __init__(self, size_x, size_y, duration, 
+                 inflow_padding=50, inflow_duration=60, inflow_radius=8, inflow_velo=1, inflow_count=5):
+        self.resolution = size_y, size_x
+        self.duration = duration
+        self.inflow_duration = inflow_duration
 
-        self.main()
+        print('Generating fluid solver, this may take some time.')
+        self.fluid = Fluid(self.resolution, 'dye')
 
-    def evolve(self):
+        center = np.floor_divide(self.resolution, 2)
+        r = np.min(center) - inflow_padding
 
-        # Simulate fluid dynamics (simple random movement)
-        self.grid += 0.01 * np.random.randn(self.width, self.height, 3)
+        points = np.linspace(-np.pi, np.pi, inflow_count, endpoint=False)
+        points = tuple(np.array((np.cos(p), np.sin(p))) for p in points)
+        normals = tuple(-p for p in points)
+        points = tuple(r * p + center for p in points)
 
-        # Clamp values to [0, 1]
-        density = np.clip(self.grid, 0, 1)
+        self.inflow_velocity = np.zeros_like(self.fluid.velocity)
+        self.inflow_dye = np.zeros(self.fluid.shape)
+        for p, n in zip(points, normals):
+            mask = np.linalg.norm(self.fluid.indices - p[:, None, None], axis=0) <= inflow_radius
+            self.inflow_velocity[:, mask] += n[:, None] * inflow_velo
+            self.inflow_dye[mask] = 1
 
-        # Map density to color and draw on the screen
-        self.screen = (density * 255).astype(np.uint8)
-        return pygame.surfarray.make_surface(self.screen)
-    
     def main(self):
-        # Initialize Pygame
-        pygame.init()
+        frames = []
+        for f in range(self.duration):
+            print(f'Computing frame {f + 1} of {self.duration}.')
+            if f <= self.inflow_duration:
+                self.fluid.velocity += self.inflow_velocity
+                self.fluid.dye += self.inflow_dye
 
-        # Create Pygame window
-        display = pygame.display.set_mode((self.width * self.pixels_per_led, self.height * self.pixels_per_led))
-        pygame.display.set_caption("Fluid Sim")
+            curl = self.fluid.step()[1]
+            # Using the error function to make the contrast a bit higher. 
+            # Any other sigmoid function e.g. smoothstep would work.
+            curl = (erf(curl * 2) + 1) / 4
 
-        # Initialize default screen
-        screen = pygame.surfarray.make_surface(np.zeros((self.width, self.height, 3)))
+            color = np.dstack((curl, np.ones(self.fluid.shape), self.fluid.dye))
+            color = (np.clip(color, 0, 1) * 255).astype('uint8')
+            frames.append(Image.fromarray(color, mode='HSV').convert('RGB'))
 
-        # ---- Main loop ----
-        clock = pygame.time.Clock()
-        running = True
+        print('Saving simulation result.')
+        frames[0].save('example.gif', save_all=True, append_images=frames[1:], duration=20, loop=0)
 
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+#endregion
 
-            screen = self.evolve() # all sims should use a common function if we want to go with this being the base runner
-            display.blit(pygame.transform.scale(screen, (self.width * self.pixels_per_led, self.height * self.pixels_per_led)), (0, 0))
+#region Fluid Class
 
-            # Update the display
-            pygame.display.flip()
+class Fluid:
+    def __init__(self, shape, *quantities, pressure_order=1, advect_order=3):
+        self.shape = shape
+        self.dimensions = len(shape)
 
-            # Cap the frame rate
-            clock.tick(self.FPS)
-            print(clock.get_fps())
+        # Prototyping is simplified by dynamically 
+        # creating advected quantities as needed.
+        self.quantities = quantities
+        for q in quantities:
+            setattr(self, q, np.zeros(shape))
 
-        # Quit Pygame
-        pygame.quit()
+        self.indices = np.indices(shape)
+        self.velocity = np.zeros((self.dimensions, *shape))
 
+        laplacian = operator(shape, difference(2, pressure_order))
+        self.pressure_solver = factorized(laplacian)
+        
+        self.advect_order = advect_order
+
+    def step(self):
+        # Advection is computed backwards in time as described in Stable Fluids.
+        advection_map = self.indices - self.velocity
+
+        # SciPy's spline filter introduces checkerboard divergence.
+        # A linear blend of the filtered and unfiltered fields based
+        # on some value epsilon eliminates this error.
+        def advect(field, filter_epsilon=10e-2, mode='constant'):
+            filtered = spline_filter(field, order=self.advect_order, mode=mode)
+            field = filtered * (1 - filter_epsilon) + field * filter_epsilon
+            return map_coordinates(field, advection_map, prefilter=False, order=self.advect_order, mode=mode)
+
+        # Apply advection to each axis of the
+        # velocity field and each user-defined quantity.
+        for d in range(self.dimensions):
+            self.velocity[d] = advect(self.velocity[d])
+
+        for q in self.quantities:
+            setattr(self, q, advect(getattr(self, q)))
+
+        # Compute the jacobian at each point in the
+        # velocity field to extract curl and divergence.
+        jacobian_shape = (self.dimensions,) * 2
+        partials = tuple(np.gradient(d) for d in self.velocity)
+        jacobian = np.stack(partials).reshape(*jacobian_shape, *self.shape)
+
+        divergence = jacobian.trace()
+
+        # If this curl calculation is extended to 3D, the y-axis value must be negated.
+        # This corresponds to the coefficients of the levi-civita symbol in that dimension.
+        # Higher dimensions do not have a vector -> scalar, or vector -> vector,
+        # correspondence between velocity and curl due to differing isomorphisms
+        # between exterior powers in dimensions != 2 or 3 respectively.
+        curl_mask = np.triu(np.ones(jacobian_shape, dtype=bool), k=1)
+        curl = (jacobian[curl_mask] - jacobian[curl_mask.T]).squeeze()
+
+        # Apply the pressure correction to the fluid's velocity field.
+        pressure = self.pressure_solver(divergence.flatten()).reshape(self.shape)
+        self.velocity -= np.gradient(pressure)
+
+        return divergence, curl, pressure
+    
+#endregion
+
+#region Helper Functions
+
+def difference(derivative, accuracy=1):
+    # Central differences implemented based on the article here:
+    # http://web.media.mit.edu/~crtaylor/calculator.html
+    derivative += 1
+    radius = accuracy + derivative // 2 - 1
+    points = range(-radius, radius + 1)
+    coefficients = np.linalg.inv(np.vander(points))
+    return coefficients[-derivative] * factorial(derivative - 1), points
+
+
+def operator(shape, *differences):
+    # Credit to Philip Zucker for figuring out
+    # that kronsum's argument order is reversed.
+    # Without that bit of wisdom I'd have lost it.
+    differences = zip(shape, cycle(differences))
+    factors = (sp.diags(*diff, shape=(dim,) * 2) for dim, diff in differences)
+    return reduce(lambda a, f: sp.kronsum(f, a, format='csc'), factors)
+
+#endregion
--- FluidSim/sim_base.py
+++ FluidSim/sim_base.py
@@ -1,10 +1,9 @@
 import numpy
-from rock_paper_scissors_sim import RockPaperScissors
-from fluid_sim import FluidSim
+from multi_state_life_sim import MultiStateLife
+import fluid_sim     # ambiguous naming scheme, settle on one sim
 from game_of_life_sim import GameOfLife
 from magma_sim import MagmaSim
-from multi_state_life_sim import MultiStateLife
-from _staging_area import fluid_sim_gif
+import Fluidsimtest1 # ambiguous naming scheme, settle on one sim
 
 '''Thoughts here are that this script acts as the pygame interpreter, 
 and we have other scripts to control different ways to mess with the image.
@@ -21,15 +20,26 @@ FPS = 60    # base fps value, each game should have their own
 game_select = numpy.random.randint(0,4) # inclusive
 game_select = 5 # hard set to test TODO: REMOVE THIS
 
-# --- RockPaperScissors --- #
+# --- RockPaperScissors/MultiStateLife --- #
 if (game_select == 0):
-    FPS = 8
-    game = RockPaperScissors(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, num_colors=4, FPS=FPS) # grid size and number of colors
+    FPS = 10
+    nh = 'Moore'
+    threshold = 3
+    rules_select = numpy.random.randint(0,2) # inclusive
+    rules_select = 2 # hard set to test TODO: REMOVE THIS
 
+    if (rules_select == 0): # 3-state (normal Rock Paper Scissor)
+        rules = {0: [2], 1: [0], 2: [1]}
+    elif (rules_select ==1): # 4-state (unbalanced)
+        rules = {1: [4], 2: [1], 3: [1, 2], 4: [2, 3]}
+    else: # (rules_select==2): # 5-state (balanced)
+        rules = {1: [4, 5], 2: [1, 5], 3: [1, 2], 4: [2, 3], 5: [3, 4]}
+    game = MultiStateLife(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS,
+                          neighbor_rules=nh, rules=rules, threshold=threshold)
 # --- Fluid Sim --- #
 elif (game_select == 1):
-    FPS = 60
-    game = FluidSim(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS)
+    game = fluid_sim.FluidSim(size_x=WIDTH, size_y=HEIGHT, duration=200)
+    game.main()
 
 # --- Basic Game of Life --- #
 elif (game_select == 2):
@@ -52,27 +62,8 @@ elif (game_select == 3):
     game = MagmaSim(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS, 
                     neighbor_rules=nh, delta_x=delta_x, starting_density=starting_density)
     
-# --- Rock Paper Scissors --- #
-elif (game_select == 4):
-    FPS = 10
-    nh = 'Moore'
-    threshold = 3
-    rules_select = numpy.random.randint(0,2) # inclusive
-    rules_select = 2 # hard set to test TODO: REMOVE THIS
 
-    if (rules_select == 0): # 3-state (normal Rock Paper Scissor)
-        rules = {0: [2], 1: [0], 2: [1]}
-    elif (rules_select ==1): # 4-state (unbalanced)
-        rules = {1: [4], 2: [1], 3: [1, 2], 4: [2, 3]}
-    else: # (rules_select==2): # 5-state (balanced)
-        rules = {1: [4, 5], 2: [1, 5], 3: [1, 2], 4: [2, 3], 5: [3, 4]}
-    game = MultiStateLife(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS,
-                          neighbor_rules=nh, rules=rules, threshold=threshold)
-
-# --- Github Fluid Sim ---
+# --- Fluidsimtest1 ---
 elif (game_select == 5):
-    #WIDTH = 500
-    #HEIGHT = 500
-    game = fluid_sim_gif.FluidSimGif(size_x=WIDTH, size_y=HEIGHT, duration=200)
-    game.main()
-
+    FPS = 60
+    game = Fluidsimtest1.FluidSim(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS)
