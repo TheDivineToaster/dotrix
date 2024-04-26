a4c8f9d7984b8d067ff7aeb7f248c280f50106a3
everetttt <ekernen2@gmail.com>
2024-04-26 14:08:15 -0500

    moved simulation chooser to function
    known issues:
    	pixels_per_inch not sending to pygame
    	radius not sending to rps

--- Visuals/sim_base.py
+++ Visuals/sim_base.py
@@ -7,6 +7,7 @@ import webbrowser
 import os
 import keyboard
 import pyautogui
+import time
 
 '''Thoughts here are that this script acts as the pygame interpreter, 
 and we have other scripts to control different ways to mess with the image.
@@ -18,10 +19,17 @@ WIDTH, HEIGHT = 60, 60 # number of "LEDs" on the screen
 pixels_per_led = 10
 FPS = 60    # base fps value, each game should have their own
 
+SIM_SELECTED = -1
+SIM_SWITCH_FREQUENCY = 20
+TIME = 0
+
 computer_vision = cv_processor(r"Visuals\weights\best.pt")
 computer_vision.start_cam(0)
 
 def get_hand_input(radius=1):
+    global TIME, SIM_SWITCH_FREQUENCY
+    if time.time() > (TIME + SIM_SWITCH_FREQUENCY):
+        choose_simulation()
     radius /= 2
     computer_vision.capture()
 
@@ -45,68 +53,76 @@ def get_hand_input(radius=1):
     return hand_list
 
 
-# Choose which sim we use
-game_select = numpy.random.randint(0,2) # inclusive
-game_select = 4
-
-# --- Basic Game of Life --- #
-if (game_select == 0):
-    FPS = 10
-    # game rules
-    B = [3]
-    S = [2, 3]
-    nh = 'Moore'
-    starting_density = 0.6
-    radius = 3
-    # run game of life
-    game = GameOfLife(size_x=WIDTH, size_y=HEIGHT, FPSs=FPS, B=B, S=S, nh=nh,
-                      cell_size=pixels_per_led, starting_density=starting_density, run_main=True, rad=radius)
-
-# --- RockPaperScissors/MultiStateLife --- #
-elif (game_select == 1):
-    FPS = 10
-    nh = 'Moore'
-    threshold = 3
-    radius = 7
-    rules_select = numpy.random.randint(0,2) # inclusive
-    rules_select = 1
-
-    if (rules_select == 0): # 3-state (normal Rock Paper Scissor)
-        rules = {1: [2], 2: [0], 3: [1]}
-    elif (rules_select ==1): # 4-state (unbalanced)
-        rules = {1: [4], 2: [1], 3: [1, 2], 4: [2, 3]}
-    else: # (rules_select==2): # 5-state (balanced)
-        rules = {1: [4, 5], 2: [1, 5], 3: [1, 2], 4: [2, 3], 5: [3, 4]}
-    game = MultiStateLife(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS,
-                          neighbor_rules=nh, rules=rules, threshold=threshold, radius=radius)
-
-# --- Magma Sim --- #
-elif (game_select == 2):
-    FPS = 10
-    nh = 'Moore'
-    delta_x = 0.01
-    starting_density = 0.5
-    radius = 7
-    cmap_options = ['viridis', 'magma', 'ocean', 'Paired', 'twilight', 'hsv']
-    colormap = cmap_options[numpy.random.randint(0, len(cmap_options) - 1)]
-    game = MagmaSim(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS, 
-                    neighbor_rules=nh, delta_x=delta_x, starting_density=starting_density, cmap=colormap, rad=radius)
+def choose_simulation():
+    global TIME, SIM_SELECTED, WIDTH, HEIGHT, FPS, pixels_per_led
+    TIME = (int)(time.time())
+    game_select = SIM_SELECTED
+    while (game_select == SIM_SELECTED):
+        game_select = numpy.random.randint(0, 8) # inclusive, choose a different sim
     
-# --- Chris Shier's javascript visuals --- #
-else:
-    screen_width, screen_height = pyautogui.size()
+    #game_select = 0 # hard set for testing
+    print("NEW GAME: " + str(game_select))
 
-    js_select = numpy.random.randint(0, 2)
-    filename = 'file:///' + os.getcwd() + '/Visuals/' + 'csh_01.html'
-    webbrowser.open_new_tab(filename)
+    # --- Basic Game of Life --- #
+    if (game_select == 0):
+        FPS = 10
+        # game rules
+        B = [3]
+        S = [2, 3]
+        nh = 'Moore'
+        starting_density = 0.6
+        radius = 3
+        # run game of life
+        game = GameOfLife(size_x=WIDTH, size_y=HEIGHT, FPSs=FPS, B=B, S=S, nh=nh,
+                        cell_size=pixels_per_led, starting_density=starting_density, run_main=True, rad=radius)
 
-    while (True):
-        if (keyboard.is_pressed("p")):
-            break
-        cursor = get_hand_input()
-        if (len(cursor) != 0):
-            cursor = cursor[0]
-            cursor_x = cursor[0] * screen_width / WIDTH
-            cursor_y = cursor[1] * screen_height / HEIGHT
-            pyautogui.moveTo(cursor_x, cursor_y, duration=.2)
+    # --- RockPaperScissors/MultiStateLife --- #
+    elif (game_select == 1):
+        FPS = 10
+        nh = 'Moore'
+        threshold = 3
+        radius = 7
+        rules_select = numpy.random.randint(0,2) # inclusive
+        rules_select = 1
+
+        if (rules_select == 0): # 3-state (normal Rock Paper Scissor)
+            rules = {1: [2], 2: [0], 3: [1]}
+        elif (rules_select ==1): # 4-state (unbalanced)
+            rules = {1: [4], 2: [1], 3: [1, 2], 4: [2, 3]}
+        else: # (rules_select==2): # 5-state (balanced)
+            rules = {1: [4, 5], 2: [1, 5], 3: [1, 2], 4: [2, 3], 5: [3, 4]}
+        game = MultiStateLife(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS,
+                            neighbor_rules=nh, rules=rules, threshold=threshold, radius=radius)
+
+    # --- Magma Sim --- #
+    elif (game_select == 2):
+        FPS = 10
+        nh = 'Moore'
+        delta_x = 0.01
+        starting_density = 0.5
+        radius = 7
+        cmap_options = ['viridis', 'magma', 'ocean', 'Paired', 'twilight', 'hsv']
+        colormap = cmap_options[numpy.random.randint(0, len(cmap_options) - 1)]
+        game = MagmaSim(size_x=WIDTH, size_y=HEIGHT, pixels_per_led=pixels_per_led, FPS=FPS, 
+                        neighbor_rules=nh, delta_x=delta_x, starting_density=starting_density, cmap=colormap, rad=radius)
+        
+    # --- Chris Shier's javascript visuals --- #
+    else:
+        screen_width, screen_height = pyautogui.size()
+
+        js_select = numpy.random.randint(0, 2)
+        filename = 'file:///' + os.getcwd() + '/Visuals/' + 'csh_01.html'
+        webbrowser.open_new_tab(filename)
+
+        while (True):
+            if (keyboard.is_pressed("p")): # exit function
+                break
+            cursor = get_hand_input()
+            if (len(cursor) != 0):
+                cursor = cursor[0]
+                cursor_x = cursor[0] * screen_width / WIDTH
+                cursor_y = cursor[1] * screen_height / HEIGHT
+                pyautogui.moveTo(cursor_x, cursor_y, duration=.2)
+    SIM_SELECTED = game_select
     
+choose_simulation()
